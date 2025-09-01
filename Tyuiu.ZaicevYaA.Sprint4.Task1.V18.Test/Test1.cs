4634e39fb64ed1aa8c3e254fe685b6928f3b5558
Redeks Gg <ddredeks@gmail.com>
2025-09-01 23:24:57 +0500

    Task1

---
+++ Tyuiu.ZaicevYaA.Sprint4.Task1.V18.Test/Test1.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tyuiu.ZaicevYaA.Sprint4.Task1.V18.Lib;
+
+namespace Tyuiu.ZaicevYaA.Sprint4.Task1.V18.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestCalculate()
+        {
+            // Arrange
+            DataService service = new DataService();
+            int[] array = { 7, 6, 9, 8, 9, 7, 4, 7, 8, 5, 6, 3, 9, 7, 9, 7 };
+            int expected = 79;
+
+            // Act
+            int actual = service.Calculate(array);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
